88614cc4bca4868d56ffec340a1a7a928479e3f9
Subhash75 <subhash.ghosh90@gmail.com>
2025-04-09 21:06:32 +0530

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "react/react-in-jsx-scope": "off", // not needed with React 17+
+    "react/prop-types": "off",
   },
   settings: {
     react: {
